4c47cb27835f8dff33da9c8e423d230e857f93c2
Victor Amaya <vamaya@asesoftware.com>
2020-05-07 11:30:11 -0500

    ajustes documentación codigo

--- robot-client/src/main/java/com/informatica/cliente/logica/HiloEscuchaBrazoServidor.java
+++ robot-client/src/main/java/com/informatica/cliente/logica/HiloEscuchaBrazoServidor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.informatica.cliente.logica;
 
 import com.informatica.cliente.presentacion.Modelo;
@@ -12,186 +7,163 @@ import java.io.IOException;
 import java.net.Socket;
 
 /**
- *
- * @author DELL
+ * Clase de tipo "Hilo" que permite recibir los mensajes que envia el servidorlos clientes
+ * @author Vamaya
  */
 public class HiloEscuchaBrazoServidor extends Thread {
 
-    private final String raizLog;
-    //private Modelo modelo;
-    private Socket servidor;
-    private Modelo modelo;
-    
-    private String nombreSlider;
-    private Integer valorSlider;
-    
-    private final boolean lecturaActiva;
-    
-    
-    /**
-     * Variables para recibir datos del servidor
-     */
-    private DataInputStream datosEntrada;
-    
-    public static final int MAX_BYTES = 200; //Tamaño máximo del mensaje
+    //VARIABLES
+    private final String raizLog;//String para guardar el nombre del hilo e identificar por consola quien está atentiendo la solicitud
+    private final Modelo modelo;//Se crea una unión con el modelo para poder modificar el dibujo del brazo
+    private final Socket servidor;//Socket del SERVIDOR, enviado por el CLIENTE
+    private DataInputStream datosEntrada;//Variable para obtener datos del servidor
+    private String nombreSlider;//Alamacenar en la clase el nombre del Slider moificado
+    private Integer valorSlider;//Alamacenar en la clase el valor del Slider modificado
+    public static final int MAX_BYTES = 200;//Tamaño máximo (en bytes) del mensaje que envía el cliente
+    private final boolean lecturaActiva;//Variable booleana para mantener al servidor escuchando al cliente
 
+    //CONSTRUCTOR
     public HiloEscuchaBrazoServidor(Socket servidor, Modelo modelo) {
-        //this.modelo = modelo;
         this.servidor = servidor;
         this.modelo = modelo;
         this.lecturaActiva = true;
-        this.raizLog = "["+this.getName() + "] - ";
+        this.raizLog = "[" + this.getName() + "] - ";
     }
+
+    //MÉTODOS
     
-    //Metodo que retorna la continuación de la comunicación
+    /**
+     * Metodo que permite retornar el valor de la varible booleana
+     * "lecturaActiva"
+     *
+     * @return
+     */
     public boolean isLecturaActiva() {
         return lecturaActiva;
     }
 
+    
+    
+    /**
+     * Metodo run del hilo, se ejecuta al iniciar el hilo Recibe los datos
+     * enviados por el servidor,
+     */
     @Override
     public void run() {
-//        try {
-//            
-//
-//        } catch (IOException e) {
-//            System.out.println(raizLog +"IO error in server thread");
-//        }
-
         try {
-            while(true){
-                
-            System.out.println(raizLog + "Leyendo datos de entrada...");
-            //capturar los flujos
-            datosEntrada = new DataInputStream(servidor.getInputStream());
-            //inputBufferReader = new BufferedReader(new InputStreamReader(servidor.getInputStream()));
-            //os=new PrintWriter(s.getOutputStream());
-            
-            //Se leen los datos de entrada del servidor
-            String mensajeServidor = "";
-                    
-            mensajeServidor = leerDataServidor();
-            //Se maneja el mensaje
-            String[] vectorMensaje = mensajeServidor.split(","); //Se concierte en un vector segun las comas encontradas en el mensaje
-            
-            //Nombre            
-            nombreSlider = vectorMensaje[0];
-            System.out.println(raizLog +"El Slider enviado fue : ["+nombreSlider+"]");
-            //Valor
-            valorSlider = Integer.parseInt(vectorMensaje[1]);
-            System.out.println(raizLog +"El Valor del Slider enviado fue : ["+valorSlider+"]");
-            // Para almacenar lo que llegue del servidor
-            
-            moverBrazo(nombreSlider, valorSlider);
-            
-            
+            //Ciclo de lectura de datos enviados por el cliente
+            while (true) {
+                datosEntrada = new DataInputStream(servidor.getInputStream());//Se crea conexión de lectura de datos con el SERVIDOR
+                System.out.println(raizLog + "Conexión de lectura de datos iniciada con el SERVIDOR .....");
+                //Se leen los datos de entrada del cleinte
+                String mensajeServidor = "";
+                mensajeServidor = leerDataServidor();
+                //Se maneja el mensaje y se actualizan los valores
+                String[] vectorMensaje = mensajeServidor.split(","); //Se convierte en un vector segun las comas encontradas en el mensaje
+                nombreSlider = vectorMensaje[0];
+                valorSlider = Integer.parseInt(vectorMensaje[1]);
+                //Se mueve el brazo con los valores recibidos
+                moverBrazo(nombreSlider, valorSlider);
             }
 
         } catch (IOException e) {
-
-            //readLine=; //reused String readLine for getting thread name
-            System.out.println(raizLog +"IO Error/ erver " + this.getName() + " terminated abruptly");
+            System.out.println(raizLog + "IO Error/ Server " + this.getName() + " terminated abruptly");
         } catch (NullPointerException e) {
-            //readLine=this.getName(); //reused String readLine for getting thread name
-            System.out.println(raizLog +"Server " + this.getName() + " Closed");
-        } catch (Exception ex) {
-            System.out.println(raizLog +"Algun otro error");
+            System.out.println(raizLog + "Server " + this.getName() + " Closed");
+        } catch (Exception e) {
+            System.out.println(raizLog + "Algun otro error");
         } finally {
             try {
-                System.out.println(raizLog +"Connection Closing..");
+                System.out.println(raizLog + "Cerrando conexión .....");
                 if (datosEntrada != null) {
                     datosEntrada.close();
-                    System.out.println(raizLog +" Socket Input Stream Closed");
+                    System.out.println(raizLog + "Socket Input Stream Closed");
                 }
 
-//        if(os!=null){
-//            os.close();
-//            System.out.println("Socket Out Closed");
-//        }
                 if (servidor != null) {
                     servidor.close();
-                    System.out.println(raizLog +"Socket Closed");
+                    System.out.println(raizLog + "Socket Closed");
                 }
 
-            } catch (IOException ie) {
-                System.out.println(raizLog +"Socket Close Error");
+            } catch (IOException e) {
+                System.out.println(raizLog + "Socket Close Error");
             }
         }//end finally
     }
 
-
-
-    public String leerDataServidor() throws Exception{
-
-        byte buffer[];
-        String mensaje = "";
-
-            // leo lo que envía el server
-            System.out.println(raizLog +"Esperando mensaje...");
-            
-            //Thread.sleep(100);
-            
-            buffer = new byte[MAX_BYTES];
-            datosEntrada.read(buffer); // Se queda acá, hasta que el cliente envíe algo
-            //inputBufferReader.read();
-            System.out.println(raizLog +"Capturando mensaje del servidor...");
-
-            // decodificar el mensaje
-            mensaje = new String(buffer).trim();
-
-            System.out.println(raizLog +"El mensaje enviado por el servidor fue : [" + mensaje + "]");
-
-//            if (mensaje.equals("ADIOS")) {
-//                this.lecturaActiva = false;
-//            }
-
-        return mensaje;
+    
+    
+    /**
+     * Método que permite leer los datos que envía el servidor
+     *
+     * @return
+     * @throws Exception
+     */
+    public String leerDataServidor() throws Exception {
+        byte[] buffer = new byte[MAX_BYTES];//Buffer para almacenar lo datos enviado por el cliente
+        System.out.println(raizLog + "Esperando mensaje del servidor .....");
+        datosEntrada.read(buffer);//Se queda acá, hasta que el cliente envíe algo
+        System.out.println(raizLog + "El SERVIDOR envió un mensaje, capturando mensaje .....");
+        String mensaje = new String(buffer).trim();//Variable para almacenar el mensaje enviado por el cliente
+        System.out.println(raizLog + "El mensaje enviado por el servidor fue : [" + mensaje + "]");
+        return mensaje; //e retorna el menaje recibido
     }
     
+        
     
-    
-    
+    /**
+     * Método para mover el brazo del servidor, de acuerdo con el mensaje
+     * recibido por alguno de los clientes
+     *
+     * @param nombreSlider
+     * @param valorSlider
+     */
     public void moverBrazo(String nombreSlider, Integer valorSlider) {
-
-        //Falange dig izq
-        System.out.println("Entro a cambio de estado..");
+        System.out.println(raizLog + "Se va a cambiar la posición del brao del servidor .....");
         if (nombreSlider.equals(NombreSlider.FALANGE_DIG_IZQ.toString())) {
-            System.out.println("Falange dig izq");
+            System.out.println(raizLog + "Falange dig izq");
             //Mover la falange dig izq
             modelo.girarFalDigIzq(valorSlider, true);
 
         } else if (nombreSlider.equals(NombreSlider.FALANGE_DIG_DER.toString())) {
-            System.out.println("Falange dig der");
+            System.out.println(raizLog + "Falange dig der");
             //Mover la falange dig der
             modelo.girarFalDigDer(valorSlider, true);
 
         } else if (nombreSlider.equals(NombreSlider.FALANGE_PROX_IZQ.toString())) {
-            System.out.println("Falange prox izq");
+            System.out.println(raizLog + "Falange prox izq");
             //Mover la falange prox izq
             modelo.girarFalProxIzq(valorSlider, true);
 
         } else if (nombreSlider.equals(NombreSlider.FALANGE_PROX_DER.toString())) {
-            System.out.println("Falange prox der");
+            System.out.println(raizLog + "Falange prox der");
             //Mover la falange prox der
             modelo.girarFalProxDer(valorSlider, true);
 
         } else if (nombreSlider.equals(NombreSlider.MANO.toString())) {
-            System.out.println("Mano");
+            System.out.println(raizLog + "Mano");
             //Mover la mano
             modelo.girarMano(valorSlider, true);
 
         } else if (nombreSlider.equals(NombreSlider.ANTEBRAZO.toString())) {
-            System.out.println("Antebrazo");
+            System.out.println(raizLog + "Antebrazo");
             //Mover el antebrazo
             modelo.girarAnteBrazo(valorSlider, true);
 
         } else if (nombreSlider.equals(NombreSlider.BRAZO.toString())) {
-            System.out.println("Brazo");
+            System.out.println(raizLog + "Brazo");
             //Mover el antebrazo
             modelo.girarBrazo(valorSlider, true);
 
         }
 
     }
+    
+   
+
+    
+    
+    
     
     
 }
--- robot-server/src/main/java/com/informatica/servidor/logica/HiloEscucharBrazoCliente.java
+++ robot-server/src/main/java/com/informatica/servidor/logica/HiloEscucharBrazoCliente.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.informatica.servidor.logica;
 
 import com.informatica.servidor.presentacion.Modelo;
@@ -12,221 +7,157 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
+ * Clase de tipo "Hilo" que permite recibir los mensajes que envian los clientes
  *
- * @author DELL
+ * @author Vamaya
  */
 public class HiloEscucharBrazoCliente extends Thread {
 
-    private Modelo modelo;
-   private final String raizLog;
-    
-    private DataOutputStream datosSalida;
-    private DataInputStream datosEntrada;
-    
-    private String nombreSlider;
-    private Integer valorSlider;
+    //VARIABLES
+    private final String raizLog;//String para guardar el nombre del hilo e identificar por consola quien está atentiendo la solicitud
+    private final Modelo modelo;//Se crea una unión con el modelo para poder modificar el dibujo del brazo
+    private final Socket cliente;//Socket del cliente, enviado por el servidor
+    private final BrazoServidor servidor;//Clase donde se generó la conexión del servidor
+    private DataOutputStream datosSalida;//Variable para el envío de datos al cliente
+    private DataInputStream datosEntrada;//Variable para obtener datos del cliente
+    private String nombreSlider;//Alamacenar en la clase el nombre del Slider moificado
+    private Integer valorSlider;//Alamacenar en la clase el valor del Slider modificado
+    public static final int MAX_BYTES = 200;//Tamaño máximo (en bytes) del mensaje que envía el cliente
+    private final boolean lecturaActiva;//Variable booleana para mantener al servidor escuchando al cliente
 
-    public String getNombreSlider() {
-        return nombreSlider;
-    }
-
-    public Integer getValorSlider() {
-        return valorSlider;
-    }
-    
-    
-
-    //Variables para la comunicación (Envío y recepción de datos)
-    public static final int MAX_BYTES = 200; //Tamaño máximo del mensaje
-
-    //Variables para el hilo de trabajo
-    private final boolean lecturaActiva;
-
-//    String readLine=null;
-//    BufferedReader  inputBufferReader = null;
-    //PrintWriter os=null;
-    private final Socket cliente;
-
-    public Socket getCliente() {
-        return cliente;
-    }
-    
-    
-    
-    private final BrazoServidor servidor;
-
+    //CONSTRUCTOR
     public HiloEscucharBrazoCliente(Socket cliente, Modelo modelo, BrazoServidor servidor) {
         this.cliente = cliente;
         this.modelo = modelo;
         this.servidor = servidor;
         this.lecturaActiva = true;
-        this.raizLog = "["+this.getName() + "] - ";
+        this.raizLog = "[" + this.getName() + "] - ";
     }
 
-    //Metodo que retorna la continuación de la comunicación
+    //MÉTODOS
+    /**
+     * Metodo que permite retornar el valor de la varible booleana
+     * "lecturaActiva"
+     *
+     * @return
+     */
     public boolean isLecturaActiva() {
         return lecturaActiva;
     }
 
+    /**
+     * Metodo run del hilo, se ejecuta al iniciar el hilo Recibe los datos
+     * enviados por el cliente,
+     */
     @Override
     public void run() {
-//        try {
-//            
-//            //capturar los flujos
-//            
-//            //os=new PrintWriter(s.getOutputStream());
-//
-//        } catch (IOException e) {
-//            System.out.println(raizLog +"IO error in server thread");
-//        }
-
         try {
-            while(true){
-            System.out.println(raizLog + "Leyendo datos de entrada...");
-            datosEntrada = new DataInputStream(cliente.getInputStream());
-            
-            //Se leen los datos de entrada del cleinte
-            String mensajeCliente = "";
-                    
-            mensajeCliente = leerDataCliente();
-            //Se maneja el mensaje
-            String[] vectorMensaje = mensajeCliente.split(","); //Se concierte en un vector segun las comas encontradas en el mensaje
-
-            Boolean error;
-            int i = 0;
-            do{
-                try {
-                    error = false;
-                    //Valor
-                    valorSlider = Integer.parseInt(vectorMensaje[i + 1]);
-                    System.out.println(raizLog + "El Valor del Slider enviado fue : [" + valorSlider + "]");
-                    //Nombre            
-                    nombreSlider = vectorMensaje[i];
-                    System.out.println(raizLog + "El Slider enviado fue : [" + nombreSlider + "]");
-                } catch (Exception e) {
-                    error = true;
-                    i++;
-                }
-            }while(error);
-            
-            moverBrazo(nombreSlider, valorSlider);
-            
-            
-            actualizarMensajeClientes();
-            
+            //Ciclo de lectura de datos enviados por el cliente
+            while (true) {
+                datosEntrada = new DataInputStream(cliente.getInputStream());//Se crea conexión de lectura de datos con el cliente
+                System.out.println(raizLog + "Conexión de lectura de datos iniciada con el cliente .....");
+                //Se leen los datos de entrada del cleinte
+                String mensajeCliente = "";
+                mensajeCliente = leerDataCliente();
+                //Se maneja el mensaje y se actualizan los valores
+                String[] vectorMensaje = mensajeCliente.split(","); //Se convierte en un vector segun las comas encontradas en el mensaje
+                nombreSlider = vectorMensaje[0];
+                valorSlider = Integer.parseInt(vectorMensaje[1]);
+                //Se mueve el brazo con los valores recibidos
+                moverBrazo(nombreSlider, valorSlider);
+                //Se envía el datos a los demás clientes conectados
+                actualizarMensajeClientes();
             }
 
         } catch (IOException e) {
-
-            //readLine=; //reused String readLine for getting thread name
-            System.out.println(raizLog +"IO Error/ Client " + this.getName() + " terminated abruptly");
+            System.out.println(raizLog + "IO Error/ Client " + this.getName() + " terminated abruptly");
         } catch (NullPointerException e) {
-            //readLine=this.getName(); //reused String readLine for getting thread name
-            System.out.println(raizLog +"Client " + this.getName() + " Closed");
-        } catch (Exception ex) {
-            System.out.println(raizLog +"Algun otro error");
+            System.out.println(raizLog + "Client " + this.getName() + " Closed");
+        } catch (Exception e) {
+            System.out.println(raizLog + "Algun otro error");
         } finally {
             try {
-                System.out.println(raizLog +"Connection Closing..");
+                System.out.println(raizLog + "Cerrando conexión .....");
                 if (datosEntrada != null) {
                     datosEntrada.close();
-                    System.out.println(raizLog +" Socket Input Stream Closed");
+                    System.out.println(raizLog + "Socket Input Stream Closed");
                 }
 
-//        if(os!=null){
-//            os.close();
-//            System.out.println("Socket Out Closed");
-//        }
                 if (cliente != null) {
                     cliente.close();
-                    System.out.println(raizLog +"Socket Closed");
+                    System.out.println(raizLog + "Socket Closed");
                 }
-                
 
-            } catch (IOException ie) {
-                System.out.println(raizLog +"Socket Close Error");
+            } catch (IOException e) {
+                System.out.println(raizLog + "Socket Close Error");
             }
         }//end finally
     }
 
-
-
-    public String leerDataCliente() throws Exception{
-
-        byte buffer[];
-        String mensaje = "";
-
-            // leo lo que envía el server
-            System.out.println(raizLog +"Esperando mensaje...");
-            
-            //Thread.sleep(100);
-            
-            buffer = new byte[MAX_BYTES];
-            datosEntrada.read(buffer); // Se queda acá, hasta que el cliente envíe algo
-            System.out.println(raizLog +"Capturando mensaje del cliente...");
-
-            // decodificar el mensaje
-            mensaje = new String(buffer).trim();
-
-            System.out.println(raizLog +"El mensaje enviado por el cliente fue : [" + mensaje + "]");
-
-//            if (mensaje.equals("ADIOS")) {
-//                this.lecturaActiva = false;
-//            }
-
-        return mensaje;
+    /**
+     * Método que permite leer los datos que envía el cliente
+     *
+     * @return
+     * @throws Exception
+     */
+    public String leerDataCliente() throws Exception {
+        byte[] buffer = new byte[MAX_BYTES];//Buffer para almacenar lo datos enviado por el cliente
+        System.out.println(raizLog + "Esperando mensaje del cliente .....");
+        datosEntrada.read(buffer);//Se queda acá, hasta que el cliente envíe algo
+        System.out.println(raizLog + "El cliente envió un mensaje, capturando mensaje .....");
+        String mensaje = new String(buffer).trim();//Variable para almacenar el mensaje enviado por el cliente
+        System.out.println(raizLog + "El mensaje enviado por el cliente fue : [" + mensaje + "]");
+        return mensaje; //e retorna el menaje recibido
     }
-    
-    
-    public void actualizarMensajeClientes() throws IOException{
+
+    /**
+     * Metodo que permite actualizar el valor de los demás clientes
+     *
+     * @throws IOException
+     */
+    public void actualizarMensajeClientes() throws IOException {
+        //Se carga la lista de hilos que tiene el servidor
         List<HiloEscucharBrazoCliente> listaHilos = servidor.getListaHilosClientes();
-        
+        //Se recorre de cada hilo
         for (HiloEscucharBrazoCliente unHilo : listaHilos) {
-            if(!unHilo.getName().equals(this.getName()) && unHilo.isAlive()){
+            //Se revisa que el hilo no sea el hilo actual (sta misma instancia, la instancia que envía el mensaje) y que además esté activo
+            if (!unHilo.getName().equals(this.getName()) && unHilo.isAlive()) {
                 //Enviar mensaje a los demás
+                System.out.println(raizLog + "Se va a actualizar el valor en los demás cliente .....");
                 unHilo.enviarMensaje(this.nombreSlider, this.valorSlider);
             }
-            
+
         }
     }
-    
-    
-    
-    
-    public void enviarMensaje(String nombreSliderEnviado, Integer ValorSliderEnviado) throws IOException{
-        
-        try {
-            Thread.sleep(200);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(HiloEscucharBrazoCliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        //Establecer el protocolo de mensaje a enviar
-        String mensaje = nombreSliderEnviado + "," + ValorSliderEnviado+",";
 
+    /**
+     * Método que permite enviar mensaje a los demás hilos
+     *
+     * @param nombreSliderEnviado
+     * @param ValorSliderEnviado
+     * @throws IOException
+     */
+    public void enviarMensaje(String nombreSliderEnviado, Integer ValorSliderEnviado) throws IOException {
+        //Establecer el protocolo de mensaje a enviar
+        String mensaje = nombreSliderEnviado + "," + ValorSliderEnviado + ",";
         //Capturo el flujo de salida y lo asocio al dato de salida
         datosSalida = new DataOutputStream(this.cliente.getOutputStream());
-
         //Opero con los mensajes
-        System.out.println(raizLog + "Enviando el mensaje: [" + mensaje + "]");
+        System.out.println(raizLog + "Enviando a los clientes el mensaje: [" + mensaje + "]");
         datosSalida.write(mensaje.getBytes()); // ESTE ES EL PROTOCOLO
-//        try {
-//            Thread.sleep(100);
-//        } catch (InterruptedException ex) {
-//            System.out.println("Error");
-//        }
-        
     }
-    
 
+    /**
+     * Método para mover el brazo del servidor, de acuerdo con el mensaje
+     * recibido por alguno de los clientes
+     *
+     * @param nombreSlider
+     * @param valorSlider
+     */
     public void moverBrazo(String nombreSlider, Integer valorSlider) {
-
-        //Falange dig izq
-        System.out.println(raizLog + "Entro a cambio de estado..");
+        System.out.println(raizLog + "Se va a cambiar la posición del brao del servidor .....");
         if (nombreSlider.equals(NombreSlider.FALANGE_DIG_IZQ.toString())) {
             System.out.println(raizLog + "Falange dig izq");
             //Mover la falange dig izq
